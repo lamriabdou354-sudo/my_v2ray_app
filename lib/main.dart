7bbdcc74ad7e4e4c712dbb4d09eb2f67485fccc2
lamriabdou354-sudo <lamriabdou354@gmail.com>
2026-09-09 17:41:06 +0100

    lib/main.dart

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+name: my_v2ray_app
+description: "A new Flutter V2Ray client application."
+publish_to: 'none'
+version: 1.0.0+1
+
+environment:
+  sdk: '>=3.0.0 <4.0.0'
+
+dependencies:
+  flutter:
+    sdk: flutter
+  http: ^1.2.0
+  flutter_v2ray: ^0.8.0
+
+dev_dependencies:
+  flutter_test:
+    sdk: flutter
+  flutter_lints: ^3.0.0
+
+flutter:
+  uses-material-design: true
+
